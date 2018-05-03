6cf4521552a465902031346f84eca5cf3045105e
Simon Weller <s.meierbeck@gmail.com>
2018-05-03 11:10:20 +0200

    Adding scope for reading private playlists

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/login', function(req, res) {
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      scope: 'user-read-private user-read-email playlist-read-private',
       redirect_uri
     }))
 })
