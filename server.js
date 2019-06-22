eb09f8540e3f5cf5fc56571b0eb696ceacd55119
Cristian Martinez <2cristianmartinez@gmail.com>
2019-06-22 19:39:10 -0400

    updated accessToken and formatting /callback

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-let express = require('express')
-let request = require('request')
-let querystring = require('querystring')
+let express = require('express');
+let request = require('request');
+let querystring = require('querystring');
 
-let app = express()
+let app = express();
 
-let redirect_uri = 
-  process.env.REDIRECT_URI || 
-  'http://localhost:8888/callback'
+let redirect_uri =
+  process.env.REDIRECT_URI ||
+  'http://localhost:8888/callback';
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      scope: 'user-read-private user-read-email user-read-playback-state playlist-modify-public playlist-modify-private',
       redirect_uri
-    }))
-})
+    }));
+});
 
-app.get('/callback', function(req, res) {
-  let code = req.query.code || null
+app.get('/callback', function (req, res) {
+  let code = req.query.code || null;
   let authOptions = {
     url: 'https://accounts.spotify.com/api/token',
     form: {
@@ -33,14 +33,18 @@ app.get('/callback', function(req, res) {
       ).toString('base64'))
     },
     json: true
-  }
-  request.post(authOptions, function(error, response, body) {
-    var access_token = body.access_token
-    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
-    res.redirect(uri + '?access_token=' + access_token)
-  })
-})
+  };
+  request.post(authOptions, (error, response, body) => {
+    try {
+      let accessToken = body.access_token;
+      let uri = process.env.FRONTEND_URI || 'http://localhost:8080';
+      res.redirect(uri + '?access_token=' + accessToken);
+    } catch {
+      console.error(error)
+    }
+  });
+});
 
-let port = process.env.PORT || 8888
-console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
-app.listen(port)
+let port = process.env.PORT || 8888;
+console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`);
+app.listen(port);
