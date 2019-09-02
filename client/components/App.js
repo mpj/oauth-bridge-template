ff0c31edc7cb6ca42883efeab80f7793a4056aae
Jayvee Aspa <jayveeaspa@Jayvees-MacBook-Pro.local>
2019-09-02 10:51:59 -0700

    change to hooks/ cors problem

--- client/components/App.js
+++ client/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import SongBanner from './SongBanner.js'
 import RecentList from './RecentList.js'
 
@@ -9,94 +9,92 @@ import 'react-svg-radar-chart/build/css/index.css'
 
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.fetchCurrentPlaying = this.fetchCurrentPlaying.bind(this);
-    this.fetchAudioAnalysis = this.fetchAudioAnalysis.bind(this);
-    this.assignSpiderData = this.assignSpiderData.bind(this);
-    this.saveCurrSong = this.saveCurrSong.bind(this);
-    this.state = {
-      user: {},
-      currSong: {},
-      spiderData: {},
-      data: []
-    }
-  }
+const App = () => {
+  const [user, setUser] = useState({})
+  const [currentSong, setCurrentSong] = useState({})
+  const [spiderData, setSpiderData] = useState({})
+  const [radarData, setRadarData] = useState([])
+  useEffect(init, [])
 
-  fetchCurrentPlaying() {
+  function fetchCurrentPlaying() {
     console.log(new Date())
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
     //fetch the currently playing song for spotify user
     fetch('https://api.spotify.com/v1/me/player/currently-playing', {
       headers: { 'Authorization': 'Bearer ' + accessToken }
-    }).then(res => res.json())
-      .then(data => {
-        if (Object.keys(this.state.currSong).length === 0 || data.item.id !== this.state.currSong.item.id) {
+    }).then(res => {
+      console.log(res)
+      res.json()
+    })
+      .then(res => {
+        if (Object.keys(currentSong).length === 0 || res.item.id !== currentSong.item.id) {
           console.log('switched songs!!!')
-          this.setState({ currSong: data }, () => {
-            this.saveCurrSong();
-            this.fetchAudioAnalysis();
-          })
+          setCurrentSong(res)
+          saveCurrSong()
+          fetchAudioAnalysis()
         }
       })
       .catch(error => {
-        this.setState({ currSong: {}, data: [] })
         console.log(error)
       })
 
   }
 
-  fetchAudioAnalysis() {
+  function fetchAudioAnalysis() {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
 
-    let url = `https://api.spotify.com/v1/audio-features/${this.state.currSong.item.id}`
+    let url = `https://api.spotify.com/v1/audio-features/${currentSong.item.id}`
     fetch(url, { headers: { 'Authorization': 'Bearer ' + accessToken } })
       .then(res => res.json())
-      .then(data => {
-        this.setState({ spiderData: data })
-      }).then(() => this.assignSpiderData())
-      .catch(error => { console.log(error) })
+      .then(res => {
+        setSpiderData(res)
+        assignSpiderData()
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 
-  assignSpiderData() {
+  function assignSpiderData() {
     const mockData = [
       {
         data: {
-          danceability: this.state.spiderData.danceability || 0.5,
-          valence: this.state.spiderData.valence || 0.5,
-          energy: this.state.spiderData.energy || 0.5,
-          popularity: this.state.currSong.item.popularity / 100 || 0.5,
-          acousticness: this.state.spiderData.acousticness || 0.5,
+          danceability: spiderData.danceability || 0.5,
+          valence: spiderData.valence || 0.5,
+          energy: spiderData.energy || 0.5,
+          popularity: currSong.item.popularity / 100 || 0.5,
+          acousticness: spiderData.acousticness || 0.5,
         },
         meta: { color: 'green' }
       },
     ];
     //populate/init data to be used by RadarChart
-    this.setState({ data: mockData });
-    console.log(this.state)
+    setRadarData(mockData)
   }
 
-  saveCurrSong() {
+  function saveCurrSong() {
     if (localStorage.getItem('savedSongs') === null) localStorage.setItem('savedSongs', JSON.stringify([]))
     const savedSongs = JSON.parse(localStorage.getItem('savedSongs')) //parse saved songs array
-    savedSongs.push(this.state.currSong.item.name)
+    savedSongs.push(currentSong.item.name)
     if (savedSongs.length === 6) savedSongs.shift()
     localStorage.setItem('savedSongs', JSON.stringify(savedSongs))
     console.log('saved item to local storage!')
   }
 
-  componentWillMount() {
+
+  function init() {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
 
     fetch('https://api.spotify.com/v1/me', {
       headers: { 'Authorization': 'Bearer ' + accessToken }
     }).then(res => res.json())
-      .then(data => this.setState({ user: data }))
-      .then(() => this.fetchCurrentPlaying())
+      .then(res => {
+        setUser(res)
+        fetchCurrentPlaying()
+      })
       .catch(error => console.log(error))
   }
 
@@ -106,47 +104,48 @@ class App extends Component {
   // }
 
 
-  render() {
+  const captions = {
+    // columns
+    danceability: 'Danceability',
+    valence: 'Valence',
+    energy: 'Energy',
+    acousticness: 'Acousticness',
+    popularity: 'Popularity'
+  };
 
-    const captions = {
-      // columns
-      danceability: 'Danceability',
-      valence: 'Valence',
-      energy: 'Energy',
-      acousticness: 'Acousticness',
-      popularity: 'Popularity'
-    };
+  // let albumImg
+  // if (Object.keys(currentSong).length === 0) albumImg = null
+  // else albumImg = <img id='albumImage' src={currentSong.item.album.images[0].url} width='300' length='300' />
 
-    let albumImg
-    if (Object.keys(this.state.currSong).length === 0) albumImg = null
-    else albumImg = <img id='albumImage' src={this.state.currSong.item.album.images[0].url} width='300' length='300' />
+  //trying to extract all the artists
+  // let artistsArr
+  // if (Object.keys(this.state.currSong).length === 0) artistsArr = []
+  // else {
+  //   artistsArr = this.state.currSong.item.artists.map(element => {
+  //     return element.name
+  //   })
+  // }
 
-    //trying to extract all the artists
-    // let artistsArr
-    // if (Object.keys(this.state.currSong).length === 0) artistsArr = []
-    // else {
-    //   artistsArr = this.state.currSong.item.artists.map(element => {
-    //     return element.name
-    //   })
-    // }
+  return (
+    <div>
+      {/* <h2>Welcome, {this.state.user.display_name}</h2> */}
 
-    return (
-      <div>
-        {/* <h2>Welcome, {this.state.user.display_name}</h2> */}
+      {Object.keys(currentSong).length === 0 ? <span id='noSong'>No Song Currently Playing...</span> : <SongBanner currSong={currentSong} />}
 
-        {Object.keys(this.state.currSong).length === 0 ? <span id='noSong'>No Song Currently Playing...</span> : <SongBanner currSong={this.state.currSong} />}
+      <br></br>
 
-        <br></br>
-        {albumImg}
-        <RadarChart captions={captions} data={this.state.data} size={300} />
-        <br></br>
-        <button onClick={this.fetchCurrentPlaying}>Fetch Currently Playing</button>
+      {Object.keys(currentSong).length === 0 ? <h1>sup</h1> : <img id='albumImage' src={currentSong.item.album.images[0].url} width='300' length='300' />}
 
-        {/* <RecentList /> */}
+      <RadarChart captions={captions} data={radarData} size={300} />
 
-      </div>
-    );
-  }
+      <br></br>
+
+      <button onClick={fetchCurrentPlaying}>Fetch Currently Playing</button>
+
+      {/* <RecentList /> */}
+
+    </div>
+  );
 }
 
 
